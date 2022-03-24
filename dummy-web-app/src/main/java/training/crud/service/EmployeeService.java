82e0cf16f68e9856075c1eb7d7cecb2f867d52ad
udham-mander <udham.mander@pkware.com>
2022-03-24 17:16:51 +0530

    dummy project for CRUD using spring boot and spring jpa ( just Repository annotation )

---
+++ dummy-web-app/src/main/java/training/crud/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package training.crud.service;
+
+import java.util.List;
+
+import training.crud.entity.Employee;
+
+public interface EmployeeService {
+
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+	
+}
---
+++ dummy-web-app/src/test/java/com/pkware/dummywebapp/DummyWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkware.dummywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
